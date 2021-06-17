1c04f51ed3d06e10986c9bdfeadad82ccdc5ae4d
mandolyte <cecil.new@gmail.com>
2021-06-17 12:18:57 -0400

    added csv and filename filter

--- shaversioning.go
+++ shaversioning.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 )
 
@@ -25,6 +27,9 @@ func main() {
 	baseUrl := "https://qa.door43.org/api/v1/repos/unfoldingword"
 	log.Println("Start.")
 
+	var results [][]string
+	header := []string{"Repo", "Release", "Filename", "SHA"}
+	results = append(results, header)
 	for rv := range rversions {
 		//log.Printf("Working on verison:%v", rversions[rv])
 		fullUrl := baseUrl + "/" + *repo + "/git/trees/" + rversions[rv]
@@ -51,11 +56,48 @@ func main() {
 					log.Fatalln("failed to cast")
 				}
 				for _, tval := range v {
+					var row []string
 					nodeMap := tval.(map[string]interface{})
-					fmt.Printf("%v,%v,%v,%v\n", *repo, rversions[rv], nodeMap["path"], nodeMap["sha"])
+					//fmt.Printf("%v,%v,%v,%v\n", *repo, rversions[rv], nodeMap["path"], nodeMap["sha"])
+					row = append(row, *repo, rversions[rv], fmt.Sprintf("%v", nodeMap["path"]), fmt.Sprintf("%v", nodeMap["sha"]))
+					results = append(results, row)
 				}
 			}
 		}
 	}
+
+	// remove any entries that are not TSV files (need to adjust this for markdown content)
+	// identify rows to remove
+	var positionsToRemove []int
+	for i := 1; i < len(results); i++ {
+		if !strings.HasSuffix(results[i][2], ".tsv") {
+			positionsToRemove = append(positionsToRemove, i)
+		}
+	}
+	// now remove them
+	for i := len(positionsToRemove) - 1; i > -1; i-- {
+		results = remove(results, positionsToRemove[i])
+	}
+
+	output := *repo + "_revs.csv"
+	f, err := os.Create(output)
+	defer f.Close()
+
+	if err != nil {
+		log.Fatalln("failed to open file", err)
+	}
+
+	w := csv.NewWriter(f)
+	err = w.WriteAll(results) // calls Flush internally
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//fmt.Println(results)
 	log.Println("Done.")
+}
+
+func remove(slice [][]string, s int) [][]string {
+	return append(slice[:s], slice[s+1:]...)
 }
