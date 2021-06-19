bbb09ec7c0fe9ec38181674d5db347e8db08a1e9
mandolyte <cecil.new@gmail.com>
2021-06-19 12:14:34 -0400

    mod

--- run_tn.sh
+++ run_tn.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 go run shaversioning.go -r en_tn \
-    -rv v13,v14,v15,v16,v17,v18,v19,v20,v21,v22,v23,v24,v25,v26,v27,v28,v29,v30,v31,v32,v33,v34,v35,v36,v37,v38,v39,v40,v41,v42,v43,v44,v45,v46,v47 \
+    -rv v13,v14,v15,v16,v17,v18,v19,v20,v21,v22,v23,v24,v25,v26,v27,v28,v29,v30,v31,v32,v33,v34,v35,v36,v37,v38,v39,v40,v41,v42,v43,v44,v45,v46,v47,v48 \
     > en_tn_revs.csv
 
 
